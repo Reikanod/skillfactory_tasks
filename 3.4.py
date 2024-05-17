ee7c79631f6555011cd824b3b23a547052471a45
Reikanod <reikanod@yandex.ru>
2024-05-17 11:30:33 +0300

    commit2

--- 3.4.py
+++ 3.4.py
@@ -1,7 +1,10 @@
-file = open("file.txt", 'a')
+text = """a long time ago
+one guy saw an owl.
+An owl was so intellegent
+so the guy felt himself too stupid around it"""
+file = open(r"additional_files\input.txt", "w")
+file.write(text)
+file = open(r"additional_files\input.txt", "r")
+out_file = open(r"additional_files\output.txt", "w")
+out_file.write(file.read())
 
-seq = ["1 строка\n", "2 string\n", "3 string\n"]
-
-file.writelines(seq)
-
-file.close()
