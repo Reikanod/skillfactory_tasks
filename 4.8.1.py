ef51f3709b5be323c057efbf59137a38327f15b0
Reikanod <reikanod@yandex.ru>
2024-05-21 13:37:30 +0300

    new task

---
+++ 4.8.1.py
@@ -0,0 +1,14 @@
+array = [2, 3, 1, 4, 6, 5, 9, 8, 7]
+count = 0
+for i in range(len(array)): # проходим по всему массиву
+
+        idx_min = i # сохраняем индекс предположительно минимального элемента
+        for j in range(i+1, len(array)):
+                if array[j] < array[idx_min]:
+                    idx_min = j
+                    count += 1
+        if i != idx_min: # если индекс не совпадает с минимальным, меняем
+            array[i], array[idx_min] = array[idx_min], array[i]
+            count += 1
+
+print(count)
