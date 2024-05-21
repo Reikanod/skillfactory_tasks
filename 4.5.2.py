99baaadca29e2900c32eab405cb3dce029edfa67
Reikanod <reikanod@yandex.ru>
2024-05-21 11:23:01 +0300

    new task

--- 4.5.2.py
+++ 4.5.2.py
@@ -32,14 +32,26 @@ D = {k : 100 for k in G.keys()}  # расстояния
 start_k = 'Адмиралтейская'  # стартовая вершина
 D[start_k] = 0  # расстояние от неё до самой себя равно нулю
 U = {k : False for k in G.keys()}  # флаги просмотра вершин
-P = {k : None for k in G.keys()}
+P = {k : None for k in G.keys()}  # предки
 
 for _ in range(len(D)):
     # выбираем среди непросмотренных наименьшее по расстоянию
     min_k = min([k for k in U.keys() if not U[k]], key = lambda x: D[x])
 
     for v in G[min_k].keys():  # проходимся по всем смежным вершинам
-        if D[v] <
-           D[v] = min(D[v], D[min_k] + G[min_k][v])  # минимум
-           P[v] = G[min_k]
+         if D[v] > D[min_k] + G[min_k][v]:  # если расстояние от текущей вершины меньше
+            D[v] = D[min_k] + G[min_k][v]  # то фиксируем его
+            P[v] = min_k  # и записываем как предок
     U[min_k] = True  # просмотренную вершину помечаем
+
+end_st = "Владимирская"
+pointer = end_st # куда должны прийти
+path = [] # список с вершинами пути
+while pointer is not None: # перемещаемся, пока не придём в стартовую точку
+   path.append(pointer)
+   pointer = P[pointer]
+
+path.reverse() # разворачиваем путь
+for v in path:
+    print(v)
+
